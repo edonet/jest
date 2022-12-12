ce580d835c435702f24f776d1ecc48ef51811902
absock <absock@163.com>
2022-12-12 23:36:09 +0800

    feat: add config

---
+++ src/config.ts
@@ -0,0 +1,39 @@
+/**
+ *****************************************
+ * Created by edonet@163.com
+ * Created on 2022-12-11 19:33:02
+ *****************************************
+ */
+'use strict';
+
+/**
+ *****************************************
+ * 加载依赖
+ *****************************************
+ */
+import { Config } from 'jest';
+import { dir, root } from './fs';
+
+
+/**
+ *****************************************
+ * 配置
+ *****************************************
+ */
+export const config: Config = {
+  rootDir: process.cwd(),
+  roots: root('test', 'packages'),
+  testMatch: ['**/?(*.)(spec|test).(ts|tsx|js|jsx)'],
+  transform: {
+    '\\.(ts|js)$': dir('./transform/tsTransform.js'),
+    '\\.(tsx|jsx)$': dir('./transform/tsxTransform.js'),
+    '\\.(scss|css|less|sass)$': dir('./transform/cssTransform.js'),
+    '\\.(html|tpl)$': dir('./transform/rawTransform.js'),
+    '^(?!.*\\.(ts|tsx|js|jsx|css|json)$)': dir('./transform/fileTransform.js'),
+  },
+  passWithNoTests: true,
+  watchPathIgnorePatterns: ['\\.temp(\\..*)?'],
+  moduleNameMapper: {
+    '^@\\/(.*)$': '<rootDir>/src/$1',
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -11,38 +11,20 @@
  * 加载依赖
  *****************************************
  */
-import { Config } from 'jest';
-import { dir, root } from './fs';
-
-
-/**
- *****************************************
- * 配置
- *****************************************
- */
-export const config: Config = {
-  rootDir: process.cwd(),
-  roots: root('test', 'packages'),
-  testMatch: ['**/?(*.)(spec|test).(ts|tsx|js|jsx)'],
-  transform: {
-    '\\.(ts|js)$': dir('./transform/tsTransform.js'),
-    '\\.(tsx|jsx)$': dir('./transform/tsxTransform.js'),
-    '\\.(scss|css|less|sass)$': dir('./transform/cssTransform.js'),
-    '\\.(html|tpl)$': dir('./transform/rawTransform.js'),
-    '^(?!.*\\.(ts|tsx|js|jsx|css|json)$)': dir('./transform/fileTransform.js'),
-  },
-  passWithNoTests: true,
-  watchPathIgnorePatterns: ['\\.temp(\\..*)?'],
-  moduleNameMapper: {
-    '^vue$': 'vue/dist/vue.common.prod.js',
-    '^@\\/(.*)$': '<rootDir>/src/$1',
-  },
-};
+import { config } from './config';
 
 
 /**
  *****************************************
  * 导出配置
+ *****************************************
+ */
+export { config };
+
+
+/**
+ *****************************************
+ * 导出默认配置
  *****************************************
  */
 export default config;
